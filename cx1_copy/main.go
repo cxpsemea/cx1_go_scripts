e23dd508ed6adf89f9aff6aaf3899d055233cccc
michael kubiaczyk <michael.kubiaczyk@checkmarx.com>
2025-04-23 10:34:54 +0200

    adding query copy script

---
+++ cx1_copy/main.go
@@ -0,0 +1,198 @@
+package main
+
+import (
+	//"crypto/tls"
+	//"flag"
+
+	"crypto/tls"
+	"encoding/json"
+	"flag"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/cxpsemea/Cx1ClientGo"
+	"github.com/sirupsen/logrus"
+	easy "github.com/t-tomalak/logrus-easy-formatter"
+)
+
+func main() {
+	os.Exit(mainRunner())
+}
+
+func mainRunner() int {
+	logger := logrus.New()
+	logger.SetLevel(logrus.InfoLevel)
+	myformatter := &easy.Formatter{}
+	myformatter.TimestampFormat = "2006-01-02 15:04:05.000"
+	myformatter.LogFormat = "[%lvl%][%time%] %msg%\n"
+	logger.SetFormatter(myformatter)
+	logger.SetOutput(os.Stdout)
+
+	logger.Info("Starting")
+
+	LogLevel := flag.String("log", "INFO", "Log level: TRACE, DEBUG, INFO, WARNING, ERROR, FATAL")
+
+	APIKey1 := flag.String("apikey1", "", "CheckmarxOne API Key (if not using client id/secret)")
+	ClientID1 := flag.String("client1", "", "CheckmarxOne Client ID (if not using API Key)")
+	ClientSecret1 := flag.String("secret1", "", "CheckmarxOne Client Secret (if not using API Key)")
+	Cx1URL1 := flag.String("cx1", "", "Optional: CheckmarxOne platform URL, if not defined in the test config.yaml")
+	IAMURL1 := flag.String("iam1", "", "Optional: CheckmarxOne IAM URL, if not defined in the test config.yaml")
+	Tenant1 := flag.String("tenant1", "", "Optional: CheckmarxOne tenant, if not defined in the test config.yaml")
+	Proxy1 := flag.String("proxy1", "", "Optional: Proxy to use when connecting to CheckmarxOne")
+
+	APIKey2 := flag.String("apikey2", "", "CheckmarxOne API Key (if not using client id/secret)")
+	ClientID2 := flag.String("client2", "", "CheckmarxOne Client ID (if not using API Key)")
+	ClientSecret2 := flag.String("secret2", "", "CheckmarxOne Client Secret (if not using API Key)")
+	Cx1URL2 := flag.String("cx2", "", "Optional: CheckmarxOne platform URL, if not defined in the test config.yaml")
+	IAMURL2 := flag.String("iam2", "", "Optional: CheckmarxOne IAM URL, if not defined in the test config.yaml")
+	Tenant2 := flag.String("tenant2", "", "Optional: CheckmarxOne tenant, if not defined in the test config.yaml")
+	Proxy2 := flag.String("proxy2", "", "Optional: Proxy to use when connecting to CheckmarxOne")
+
+	Scope := flag.String("scope", "", "Comma-separated list of items to copy: queries,presets")
+
+	flag.Parse()
+
+	switch strings.ToUpper(*LogLevel) {
+	case "TRACE":
+		logger.Info("Setting log level to TRACE")
+		logger.SetLevel(logrus.TraceLevel)
+	case "DEBUG":
+		logger.Info("Setting log level to DEBUG")
+		logger.SetLevel(logrus.DebugLevel)
+	case "INFO":
+		logger.Info("Setting log level to INFO")
+		logger.SetLevel(logrus.InfoLevel)
+	case "WARNING":
+		logger.Info("Setting log level to WARNING")
+		logger.SetLevel(logrus.WarnLevel)
+	case "ERROR":
+		logger.Info("Setting log level to ERROR")
+		logger.SetLevel(logrus.ErrorLevel)
+	case "FATAL":
+		logger.Info("Setting log level to FATAL")
+		logger.SetLevel(logrus.FatalLevel)
+	default:
+		logger.Info("Log level set to default: INFO")
+	}
+
+	if *Scope == "" {
+		logger.Fatalf("Required parameter scope is missing")
+	}
+	*Scope = strings.ToLower(*Scope)
+
+	httpClient1 := &http.Client{}
+	if *Proxy1 != "" {
+		proxyURL, err := url.Parse(*Proxy1)
+		if err != nil {
+			logger.Fatalf("Failed to parse proxy url %v: %s", *Proxy1, err)
+		}
+		transport := &http.Transport{}
+		transport.Proxy = http.ProxyURL(proxyURL)
+		transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+		httpClient1.Transport = transport
+		logger.Infof("Running with proxy1: %v", *Proxy1)
+	}
+	httpClient2 := &http.Client{}
+	if *Proxy2 != "" {
+		proxyURL, err := url.Parse(*Proxy2)
+		if err != nil {
+			logger.Fatalf("Failed to parse proxy url %v: %s", *Proxy2, err)
+		}
+		transport := &http.Transport{}
+		transport.Proxy = http.ProxyURL(proxyURL)
+		transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+		httpClient2.Transport = transport
+		logger.Infof("Running with proxy2: %v", *Proxy2)
+	}
+
+	var cx1client1, cx1client2 *Cx1ClientGo.Cx1Client
+	var err error
+
+	if *APIKey1 != "" {
+		cx1client1, err = Cx1ClientGo.NewAPIKeyClient(httpClient1, *Cx1URL1, *IAMURL1, *Tenant1, *APIKey1, logger)
+	} else {
+		cx1client1, err = Cx1ClientGo.NewOAuthClient(httpClient1, *Cx1URL1, *IAMURL1, *Tenant1, *ClientID1, *ClientSecret1, logger)
+	}
+	if err != nil {
+		logger.Fatalf("Failed to create client #1 for %v: %s", *Tenant1, err)
+	}
+	logger.Infof("Connected client #1 with %v", cx1client1.String())
+
+	if *APIKey2 != "" {
+		cx1client2, err = Cx1ClientGo.NewAPIKeyClient(httpClient2, *Cx1URL2, *IAMURL2, *Tenant2, *APIKey2, logger)
+	} else {
+		cx1client2, err = Cx1ClientGo.NewOAuthClient(httpClient2, *Cx1URL2, *IAMURL2, *Tenant2, *ClientID2, *ClientSecret2, logger)
+	}
+	if err != nil {
+		logger.Fatalf("Failed to create client #2 for %v: %s", *Tenant2, err)
+	}
+	logger.Infof("Connected client #2 with %v", cx1client2.String())
+
+	if strings.Contains(*Scope, "queries") {
+		CopyQueries(cx1client1, cx1client2, logger)
+	}
+	if strings.Contains(*Scope, "presets") {
+
+	}
+
+	return 0
+}
+
+func CopyQueries(cx1client1, cx1client2 *Cx1ClientGo.Cx1Client, logger *logrus.Logger) {
+	var err error
+	var cqc Cx1ClientGo.QueryCollection
+	var data []byte
+
+	if data, err = os.ReadFile("queries.json"); err != nil {
+		logger.Infof("Fetching queries from %v", cx1client1.String())
+
+		var collection Cx1ClientGo.QueryCollection
+		collection, err = cx1client1.GetQueries()
+		if err != nil {
+			logger.Fatalf("Failed to fetch queries from %v: %s", cx1client1.String(), err)
+		}
+
+		for _, lang := range collection.QueryLanguages {
+			logger.Infof("Fetching %v-language queries", lang.Name)
+			if err := refreshAuditSession(cx1client1, lang.Name); err != nil {
+				logger.Fatalf("Failed to refresh audit session: %v", err)
+			} else {
+				aq, err := cx1client1.GetAuditQueriesByLevelID(auditSession, cx1client1.QueryTypeProject(), testProject.ProjectID)
+				if err != nil {
+					logger.Errorf("Failed to get query code for Project-level %v queries for project %v: %s", lang.Name, testProject.String(), err)
+				} else {
+					collection.AddQueries(&aq)
+				}
+			}
+		}
+
+		if auditSession != nil {
+			deleteAuditSession(cx1client1, logger)
+		}
+
+		cqc = collection.GetCustomQueryCollection()
+
+		data, err = json.MarshalIndent(cqc, "", "  ")
+		if err != nil {
+			logger.Errorf("Failed to marshal data to json: %v", err)
+			return
+		}
+		err = os.WriteFile("queries.json", data, 0644)
+		if err != nil {
+			logger.Errorf("Failed to write data to file: %v", err)
+			return
+		}
+		logger.Infof("Saved query collection to queries.json")
+	} else {
+		logger.Infof("Loading queries from queries.json")
+		err = json.Unmarshal(data, &cqc)
+		if err != nil {
+			logger.Errorf("Failed to unmarshal data from json: %v", err)
+			return
+		}
+	}
+}
---
+++ cx1_copy/queries.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"archive/zip"
+	"bytes"
+	"fmt"
+	"io"
+
+	"github.com/cxpsemea/Cx1ClientGo"
+	"github.com/sirupsen/logrus"
+)
+
+var auditSession *Cx1ClientGo.AuditSession
+var testProject *Cx1ClientGo.Project
+var languageMap map[string]string
+
+func refreshAuditSession(cx1client *Cx1ClientGo.Cx1Client, language string) error {
+	if auditSession != nil && auditSession.HasLanguage(language) {
+		if err := cx1client.AuditSessionKeepAlive(auditSession); err != nil {
+			_ = cx1client.AuditDeleteSession(auditSession)
+			if err = createAuditSession(cx1client, language); err != nil {
+				return err
+			}
+		}
+	} else {
+		if err := createAuditSession(cx1client, language); err != nil {
+			return err
+		}
+	}
+
+	cx1client.GetAuditQueriesByLevelID(auditSession, "Corp", "Corp")
+
+	return cx1client.AuditSessionKeepAlive(auditSession)
+}
+
+func deleteAuditSession(cx1client *Cx1ClientGo.Cx1Client, logger *logrus.Logger) {
+	if auditSession == nil {
+		return
+	}
+
+	if err := cx1client.AuditDeleteSession(auditSession); err != nil {
+		logger.Errorf("Failed to delete audit session: %s", err)
+	}
+	auditSession = nil
+}
+
+func createAuditSession(cx1client *Cx1ClientGo.Cx1Client, language string) error {
+	var err error
+	if testProject == nil {
+		project, err := cx1client.GetOrCreateProjectByName("CxPSEMEA-Query Migration Project")
+		if err != nil {
+			return err
+		}
+		testProject = &project
+	}
+
+	filter := Cx1ClientGo.ScanFilter{
+		BaseFilter: Cx1ClientGo.BaseFilter{
+			Limit:  1,
+			Offset: 0,
+		},
+		Branches: []string{language},
+		Statuses: []string{"Completed"},
+	}
+	scans, err := cx1client.GetLastScansByIDFiltered(testProject.ProjectID, filter)
+	if err != nil {
+		return err
+	}
+
+	var lastscan Cx1ClientGo.Scan
+
+	if len(scans) == 0 {
+		zipFile, err := makeZip(&resourceCodeZip, language)
+		if err != nil {
+			return err
+		}
+		sastScanConfig := Cx1ClientGo.ScanConfiguration{
+			ScanType: "sast",
+		}
+
+		uploadURL, err := cx1client.UploadBytes(&zipFile)
+		if err != nil {
+			return err
+		}
+
+		lastscan, err = cx1client.ScanProjectZipByID(testProject.ProjectID, uploadURL, language, []Cx1ClientGo.ScanConfiguration{sastScanConfig}, map[string]string{})
+		if err != nil {
+			return err
+		}
+
+		lastscan, err = cx1client.ScanPollingDetailed(&lastscan)
+		if err != nil {
+			return err
+		}
+
+		if lastscan.Status != "Completed" {
+			return fmt.Errorf("scan did not complete successfully")
+		}
+	} else {
+		lastscan = scans[0]
+	}
+
+	session, err := cx1client.GetAuditSessionByID("sast", testProject.ProjectID, lastscan.ScanID)
+	if err != nil {
+		return err
+	}
+
+	auditSession = &session
+
+	return nil
+}
+
+func InitializeQueryMigration(cx1client *Cx1ClientGo.Cx1Client) {
+	languageMap = make(map[string]string)
+
+	languageMap["code/apexFile.cls"] = "Apex"
+	languageMap["code/aspFile.asp"] = "ASP"
+	languageMap["code/cobolFile.cbl"] = "Cobol"
+	languageMap["code/cplusplusFile.cpp"] = "CPP"
+	languageMap["code/csharpFile.cs"] = "CSharp"
+	languageMap["code/dartFile.dart"] = "Dart"
+	languageMap["code/Dockerfile"] = "IAST"
+	languageMap["code/goFile.go"] = "Go"
+	languageMap["code/groovyFile.groovy"] = "Groovy"
+	languageMap["code/javaFile.java"] = "Java"
+	languageMap["code/javaScriptFile.js"] = "JavaScript"
+	languageMap["code/kotlinFile.kt"] = "Kotlin"
+	languageMap["code/luaFile.lua"] = "Lua"
+	languageMap["code/objectivecFile.m"] = "Objc"
+	languageMap["code/perlFile.pl"] = "Perl"
+	languageMap["code/phpFile.php"] = "PHP"
+	languageMap["code/plsqlFile.sql"] = "PLSQL"
+	languageMap["code/pom.xml"] = "SCA"
+	languageMap["code/pythonFile.py"] = "Python"
+	languageMap["code/README.md"] = "None"
+	languageMap["code/rpgleFile.rpgle"] = "RPG"
+	languageMap["code/rubyFile.rb"] = "Ruby"
+	languageMap["code/scalaFile.scala"] = "Scala"
+	languageMap["code/swiftFile.swift"] = "Swift"
+	languageMap["code/vb6File.bas"] = "VB6"
+	languageMap["code/vbnetFile.vb"] = "VbNet"
+	languageMap["code/vbscriptFile.vbs"] = "VbScript"
+}
+
+func makeZip(zipContents *[]byte, language string) ([]byte, error) {
+	contents := new(bytes.Buffer)
+
+	zipReader, err := zip.NewReader(bytes.NewReader(*zipContents), int64(len(*zipContents)))
+	if err != nil {
+		return contents.Bytes(), err
+	}
+
+	zipWriter := zip.NewWriter(contents)
+
+	// Read all the files from zip archive
+	for _, zipFile := range zipReader.File {
+		if languageMap[zipFile.Name] == language {
+			w1, err := zipWriter.Create(zipFile.Name)
+			if err != nil {
+				return contents.Bytes(), err
+			}
+
+			f, err := zipFile.Open()
+			if err != nil {
+				return contents.Bytes(), err
+			}
+			unzippedFileBytes, err := io.ReadAll(f)
+			f.Close()
+			if err != nil {
+				return contents.Bytes(), err
+			}
+
+			if _, err := io.Copy(w1, bytes.NewReader(unzippedFileBytes)); err != nil {
+				return contents.Bytes(), err
+			}
+		}
+	}
+
+	zipWriter.Close()
+	return contents.Bytes(), nil
+}
