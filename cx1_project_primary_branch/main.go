332c774434e75940fae8cb396d54f9e0b1df4bea
michael kubiaczyk <NOREPLY_EMAIL>
2025-08-13 08:34:51 +0200

    add branch-file input for best performance

--- cx1_project_primary_branch/main.go
+++ cx1_project_primary_branch/main.go
@@ -26,6 +26,7 @@ func main() {
 	LogLevel := flag.String("log", "INFO", "Log level: TRACE, DEBUG, INFO, WARNING, ERROR, FATAL")
 	ProjectsFile := flag.String("projects", "", "Optional: file containing 1 project ID per line (otherwise check all projects)")
 	ApplicationName := flag.String("appName", "", "Optional: name of application containing projects to update")
+	BranchesFile := flag.String("branches", "", "Optional: file containing one <projectId, branchName> per line - best performance")
 	Update := flag.Bool("update", false, "Apply the change or just inform")
 	Delay := flag.Int("delay", 1000, "Delay in milliseconds between projects")
 
@@ -77,7 +78,8 @@ func main() {
 		logger.Info("This will not make any changes, only inform")
 	}
 
-	Projects := []Cx1ClientGo.Project{}
+	//Projects := []Cx1ClientGo.Project{}
+	ProjectBranches := make(map[string]string)
 
 	if *ProjectsFile != "" {
 		logger.Infof("Parsing list of project IDs from %v", *ProjectsFile)
@@ -88,15 +90,22 @@ func main() {
 		defer file.Close()
 
 		scanner := bufio.NewScanner(file)
-		pcount := 1
+		pcount := 0
 		for scanner.Scan() {
 			//logger.Infof("Read line: %v", scanner.Text())
 			ProjectID := scanner.Text()
 			project, err := cx1client.GetProjectByID(ProjectID)
 			if err != nil {
-				logger.Errorf("%d: Failed to get project %v: %v", pcount, ProjectID, err)
+				logger.Errorf("%d: Failed to get project %v: %v", pcount+1, ProjectID, err)
 			} else {
-				Projects = append(Projects, project)
+				branch, skipmsg, err := getPrimaryBranch(cx1client, &project)
+				if err != nil {
+					logger.Errorf("%d: %v", pcount+1, err)
+				} else if skipmsg != "" {
+					logger.Warningf("%d: %v", pcount+1, skipmsg)
+				} else {
+					ProjectBranches[ProjectID] = branch
+				}
 			}
 			pcount++
 		}
@@ -114,55 +123,102 @@ func main() {
 		if len(*application.ProjectIds) == 0 {
 			logger.Fatalf("Application %v has no projects", application.String())
 		}
+		logger.Infof("Application %d has %d projects", application.ApplicationID, len(*application.ProjectIds))
 
-		for id, pid := range *application.ProjectIds {
-			project, err := cx1client.GetProjectByID(pid)
+		for pcount, ProjectID := range *application.ProjectIds {
+			project, err := cx1client.GetProjectByID(ProjectID)
 			if err != nil {
-				logger.Errorf("%d: Failed to get project %v: %v", id, pid, err)
+				logger.Errorf("%d: Failed to get project %v: %v", pcount+1, ProjectID, err)
 			} else {
-				Projects = append(Projects, project)
+				branch, skipmsg, err := getPrimaryBranch(cx1client, &project)
+				if err != nil {
+					logger.Errorf("%d: %v", pcount+1, err)
+				} else if skipmsg != "" {
+					logger.Warningf("%d: %v", pcount+1, skipmsg)
+				} else {
+					ProjectBranches[ProjectID] = branch
+				}
 			}
+		}
+	} else if *BranchesFile != "" {
+		logger.Infof("Parsing list of project IDs and branches from %v", *BranchesFile)
+		file, err := os.Open(*BranchesFile)
+		if err != nil {
+			logger.Fatalf("Failed to open file: %v", err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		pcount := 0
+		for scanner.Scan() {
+			//logger.Infof("Read line: %v", scanner.Text())
+			parts := strings.Split(scanner.Text(), ",")
+			if len(parts) != 2 {
+				logger.Errorf("%d: Failed to parse line '%v', should be in <projectId,branch> format", pcount+1, scanner.Text())
+				continue
+			}
+			ProjectBranches[parts[0]] = parts[1]
+			pcount++
+		}
+
+		if err := scanner.Err(); err != nil {
+			logger.Fatal(err)
 		}
 	} else {
 		logger.Infof("Fetching all projects")
-		Projects, err = cx1client.GetAllProjects()
+		Projects, err := cx1client.GetAllProjects()
 		if err != nil {
 			logger.Fatalf("Failed to get projects: %v", err)
 		}
+		logger.Infof("Got %d projects", len(Projects))
+		for pcount, project := range Projects {
+			branch, skipmsg, err := getPrimaryBranch(cx1client, &project)
+			if err != nil {
+				logger.Errorf("%d: %v", pcount+1, err)
+			} else if skipmsg != "" {
+				logger.Warningf("%d: %v", pcount+1, skipmsg)
+			} else {
+				ProjectBranches[project.ProjectID] = branch
+			}
+		}
 	}
 
-	totalCount := len(Projects)
+	totalCount := len(ProjectBranches)
 	logger.Infof("Processing %d projects...", totalCount)
 
-	for i, project := range Projects {
-		progress := fmt.Sprintf("[#%d/%d] ", i+1, totalCount)
-		if project.MainBranch == "" {
-
-			branches, err := cx1client.GetProjectBranchesByID(project.ProjectID)
+	i := 1
+	for projectId, branch := range ProjectBranches {
+		progress := fmt.Sprintf("[#%d/%d] ", i, totalCount)
+		if *Update {
+			err = cx1client.PatchProjectByID(projectId, Cx1ClientGo.ProjectPatch{MainBranch: &branch})
 			if err != nil {
-				logger.Errorf("%vFailed to get branches for project %v: %v", progress, project.String(), err)
-			} else if len(branches) != 1 {
-				logger.Infof("%vSkipping project %v - has %d scanned branches", progress, project.String(), len(branches))
+				logger.Errorf("%vFailed to set primary branch '%v' on project %v: %v", progress, branch, projectId, err)
 			} else {
-				if *Update {
-					err = cx1client.PatchProject(&project, Cx1ClientGo.ProjectPatch{MainBranch: &branches[0]})
-					if err != nil {
-						logger.Errorf("%vFailed to set primary branch '%v' on project %v: %v", progress, branches[0], project.String(), err)
-					} else {
-						logger.Infof("%vSuccessfully set primary branch '%v' on project %v", progress, branches[0], project.String())
-					}
+				logger.Infof("%vSuccessfully set primary branch '%v' on project %v", progress, branch, projectId)
+			}
 
-					if i+1 < totalCount {
-						time.Sleep(time.Duration(*Delay) * time.Millisecond)
-					}
-				} else {
-					logger.Infof("%vWould update project %v by setting the primary branch to '%v'", progress, project.String(), branches[0])
-				}
+			if i+1 < totalCount {
+				time.Sleep(time.Duration(*Delay) * time.Millisecond)
 			}
 		} else {
-			logger.Infof("%vSkipping project %v - already has primary branch '%v'", progress, project.String(), project.MainBranch)
+			logger.Infof("%vWould update project %v by setting the primary branch to '%v'", progress, projectId, branch)
 		}
 	}
 
 	logger.Infof("Done")
+}
+
+func getPrimaryBranch(cx1client *Cx1ClientGo.Cx1Client, project *Cx1ClientGo.Project) (string, string, error) {
+	if project.MainBranch != "" {
+		return "", fmt.Sprintf("Skipping project %v - already has primary branch '%v'", project.String(), project.MainBranch), nil
+	}
+
+	branches, err := cx1client.GetProjectBranchesByID(project.ProjectID)
+	if err != nil {
+		return "", "", fmt.Errorf("failed to get branches for project %v: %v", project.String(), err)
+	} else if len(branches) != 1 {
+		return "", fmt.Sprintf("Skipping project %v - has %d scanned branches", project.String(), len(branches)), nil
+	}
+
+	return branches[0], "", nil
 }
